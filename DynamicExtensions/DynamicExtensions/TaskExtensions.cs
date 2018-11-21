e236e98c0eba9dcfd17c64d7fdd69fbc0b2ab5f5
koskokos <kostya.shnypir@gmail.com>
2018-11-21 14:58:31 +0200

    some TaskExtensions

---
+++ DynamicExtensions/DynamicExtensions/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicExtensions
+{
+    public static class TaskExtensions
+    {
+        public static Task<IEnumerable<T>> Merge<T>(this IEnumerable<Task<IEnumerable<T>>> items) => Task.WhenAll(items).Then(kek => kek.SelectMany(i => i));
+
+        public static Task<IEnumerable<T>> Distinct<T>(this Task<IEnumerable<T>> items) => items.Then(Enumerable.Distinct);
+
+        public static Task<T[]> ToArray<T>(this Task<IEnumerable<T>> items) => items.Then(Enumerable.ToArray);
+
+        public static async Task<TOut> Then<TIn, TOut>(this Task<TIn> item, Func<TIn, TOut> mapper) => mapper(await item);
+    }
+}
