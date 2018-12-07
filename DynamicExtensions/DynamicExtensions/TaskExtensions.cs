7cfd4cda7e4918c033cc3566f29454636bdf4805
koskokos <kostya.shnypir@gmail.com>
2018-12-07 09:33:19 +0200

    rename Then to Map, new Then returning nongeneric Task, AsTask extension

--- DynamicExtensions/DynamicExtensions/TaskExtensions.cs
+++ DynamicExtensions/DynamicExtensions/TaskExtensions.cs
@@ -8,13 +8,18 @@ namespace DynamicExtensions
 {
     public static class TaskExtensions
     {
-        public static Task<IEnumerable<T>> Merge<T>(this IEnumerable<Task<IEnumerable<T>>> items) => Task.WhenAll(items).Then(kek => kek.SelectMany(i => i));
+        public static Task<IEnumerable<T>> Merge<T>(this IEnumerable<Task<IEnumerable<T>>> items) => Task.WhenAll(items).Map(kek => kek.SelectMany(i => i));
 
-        public static Task<IEnumerable<T>> Distinct<T>(this Task<IEnumerable<T>> items) => items.Then(Enumerable.Distinct);
+        public static Task<IEnumerable<T>> Distinct<T>(this Task<IEnumerable<T>> items) => items.Map(Enumerable.Distinct);
 
-        public static Task<T[]> ToArray<T>(this Task<IEnumerable<T>> items) => items.Then(Enumerable.ToArray);
+        public static Task<T[]> ToArray<T>(this Task<IEnumerable<T>> items) => items.Map(Enumerable.ToArray);
 
-        public static async Task<TOut> Then<TIn, TOut>(this Task<TIn> item, Func<TIn, TOut> mapper) => mapper(await item);
-        public static async Task<TOut> Then<TIn, TOut>(this Task<TIn> item, Func<TIn, Task<TOut>> mapper) => await mapper(await item);
+        public static async Task<TOut> Map<TIn, TOut>(this Task<TIn> item, Func<TIn, TOut> mapper) => mapper(await item);
+        public static async Task<TOut> Map<TIn, TOut>(this Task<TIn> item, Func<TIn, Task<TOut>> mapper) => await mapper(await item);
+
+        public static async Task Then<TIn>(this Task<TIn> item, Action<TIn> action) => action(await item);
+        public static async Task Then<TIn>(this Task<TIn> item, Func<TIn, Task> action) => await action(await item);
+
+        public static Task<T> AsTask<T>(this T @object) => Task.FromResult(@object);
     }
 }
