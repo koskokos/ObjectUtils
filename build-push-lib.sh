b6a906b7be4d4bb5930973747931fcc43b98f7ee
koskokos <kostya.shnypir@gmail.com>
2019-07-12 12:56:11 +0300

    build to github nuget package manager

--- build-push-lib.sh
+++ build-push-lib.sh
@@ -2,7 +2,7 @@ set -ex
 git pull
 build=$((`cat .build`+1))
 version=`cat .version`.$build
-docker build --network=nuget-nw --build-arg Version=$version --build-arg NuGetApiKey=$1 -f docker-build/Dockerfile ./src/
+docker build --network=nuget-nw --build-arg GitHubNugetToken=$2 --build-arg Version=$version -f docker-build/Dockerfile ./src/
 echo $build > .build
 git add .build
 git commit -m 'build increment' --author='kos-ci <koskokos.dev@gmail.com>'
