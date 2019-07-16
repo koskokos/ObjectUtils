88d32c9e12de058d38a7baad502289b7a9cd87c8
koskokos <kostya.shnypir@gmail.com>
2019-07-16 09:11:37 +0300

    new build

--- build-push-lib.sh
+++ build-push-lib.sh
@@ -2,7 +2,7 @@ set -ex
 git pull
 build=$((`cat .build`+1))
 version=`cat .version`.$build
-docker build --network=nuget-nw --build-arg GitHubNugetToken=$2 --build-arg Version=$version -f docker-build/Dockerfile ./src/
+docker build --network=nuget-nw --build-arg NuGetSource=$1 --build-arg NuGetApiKey=$2 --build-arg Version=$version -f docker-build/Dockerfile ./src/
 echo $build > .build
 git add .build
 git commit -m 'build increment' --author='kos-ci <koskokos.dev@gmail.com>'
---
+++ build-push-release-lib.bat
@@ -0,0 +1,17 @@
+set /P build=<.build
+set /P version=<.version
+set version=%version%.%build%
+cd src
+set NuGetSource=%1
+set NuGetApiKey=%2
+
+dotnet build -c Release -p:Version=%version% ./src/MiscellaneousUtils.sln
+dotnet test ./src/MiscellaneousUtils.Tests/MiscellaneousUtils.Tests.csproj --no-build -c Release
+dotnet pack ./MiscellaneousUtils/MiscellaneousUtils.csproj --no-build -c Release -o /out -p:NuGetVersion=%version%
+
+dotnet nuget push -s %NuGetSource% -k %NuGetApiKey% /out/MiscellaneousUtils.%version%.nupkg
+
+REM echo $build > .build
+REM git add .build
+REM git commit -m 'build increment' --author='kos-ci <koskokos.dev@gmail.com>'
+REM git push
